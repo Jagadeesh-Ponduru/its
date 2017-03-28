41930f7aa13b22361bf3880c26d65db182f97d7b
Jagadeesh Ponduru <Jagadeesh-Ponduru@users.noreply.github.com>
2017-03-28 13:31:23 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Sensor(models.Model):#Stores information about sensor values
+    Sensor_name      = models.CharField(max_length=200,default=None)
+    Sensor_value        = models.CharField(max_length=200,default=None)
+    def __str__(self):
+        return '@'+self.Sensor_name
+      
