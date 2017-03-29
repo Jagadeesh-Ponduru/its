3852ffe3003d1f233fb999f69506775ba5098a37
Jagadeesh Ponduru <Jagadeesh-Ponduru@users.noreply.github.com>
2017-03-29 14:26:08 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -4,9 +4,4 @@ from django.db import models
 
 # Create your models here.
 
-class Sensor(models.Model):#Stores information about sensor values
-    Sensor_name      = models.CharField(max_length=200,default=None)
-    Sensor_value        = models.CharField(max_length=200,default=None)
-    def __str__(self):
-        return '@'+self.Sensor_name
       
